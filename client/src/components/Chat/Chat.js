5bbad95c8f3840ca62ccdf3eac2916f307c9877f
Rhonda Kremer <rhondakremer@gmail.com>
2020-03-29 14:59:21 -0400

    endpoint updated with heroku backend and netlify deploy

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -18,7 +18,7 @@ const Chat = ({ location }) => {
     const [users, setUsers] = useState('');
     const [message, setMessage] = useState('');
     const [messages, setMessages] = useState([]);
-    const ENDPOINT = 'localhost:5000';
+    const ENDPOINT = 'https://react-chat-app-rhonda.herokuapp.com/';
 
     useEffect(() => {
         const { name, room } = queryString.parse(location.search)
